86e98a0d957237467ddb8419d3eecb1b8df0073d
Rafal Kuruc <rafalkuruc@gmail.com>
2022-12-04 13:01:39 +0100

    Initial commit - levelTool implementation using e-compas

---
+++ Debug/Drivers/BSP/STM32F411E-Discovery/subdir.mk
@@ -0,0 +1,23 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery.c \
+../Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery_accelerometer.c 
+
+OBJS += \
+./Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery.o \
+./Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery_accelerometer.o 
+
+C_DEPS += \
+./Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery.d \
+./Drivers/BSP/STM32F411E-Discovery/stm32f411e_discovery_accelerometer.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/BSP/STM32F411E-Discovery/%.o: ../Drivers/BSP/STM32F411E-Discovery/%.c Drivers/BSP/STM32F411E-Discovery/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F411xE -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
